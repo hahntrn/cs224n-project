b8b0e45f4306ccf5859c008b5406178d50d56279
Andrea <acoll3@stanford.edu>
2021-03-08 17:49:52 -0800

    removed some print statements

--- train.py
+++ train.py
@@ -119,12 +119,12 @@ def prepare_train_data(args, dataset_dict, tokenizer, augment_dataset_dicts=None
                         selected_sentence = sentences[top_results[0]]
 
                         # selected_sentence = sentences[torch.argmax(cosine_sim_sentences)]
-                        print(f"--selected_sentence: {selected_sentence}")
-                        print(f"--Most relevant to: {selected_context}")
+                        # print(f"--selected_sentence: {selected_sentence}")
+                        # print(f"--Most relevant to: {selected_context}")
                         selected_context = selected_sentence # should call this demonstration
 
                     dataset_dict['context'][context_i] += ' ' + tokenizer.sep_token + ' ' + selected_context
-                    print("selected_context",selected_context)
+                    # print("selected_context",selected_context)
 
                 # best_demonstrations = [
                 #                 augment_dataset_dict['context'][
